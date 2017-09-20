1d9ac5e1e2e939cf99fc4f31f2f46632476850fc
Tobias Patton <tobias.patton@semaphoresolutions.ca>
2017-09-20 11:43:48 -0700

    Create instances of the interfaces in index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,21 @@
 import { MyInterface, BAR } from "./MyInterface";
 import { MyOtherInterface, FOO } from "./MyOtherInterface"
 
+class MyInstance implements MyInterface {
+    doIt() {
+        throw new Error("Method not implemented.");
+    }
+}
+
+class MyOtherInstance implements MyOtherInstance {
+    doIt() {
+        throw new Error("Method not implemented.");
+    }
+}
+
 class main {
     foo = FOO;
+    bar = BAR;
+    inst = new MyInstance();
+    inst2 = new MyOtherInstance();
 }
